c0b860259bcd1849a5df4e0c9bbbdeb481d1f418
welshej <welshej@gmail.com>
2014-09-29 23:48:21 -0400

    comments up the wazoo

--- ProducerConsumer/ProducerConsumer/prodcons.c
+++ ProducerConsumer/ProducerConsumer/prodcons.c
@@ -3,9 +3,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-
 typedef struct cs1550_sem semaphore;
-
 struct cs1550_sem
 {
     int value;
@@ -14,18 +12,18 @@ struct cs1550_sem
 };
 
 
-
-struct cs1550_sem *empty;
-struct cs1550_sem *full;
-struct cs1550_sem *mutex;
-
-
-int *buffer;
-int num_of_elements;
-
+// PROTOTYPES
 void down(struct cs1550_sem *);
 void up(struct cs1550_sem *);
 
+
+//globals
+struct cs1550_sem *empty;
+struct cs1550_sem *full;
+struct cs1550_sem *mutex;
+ //-----------------------//
+int *buffer;
+int num_of_elements;
 
 
 int main (int argc, char *argv[])
@@ -36,27 +34,26 @@ int main (int argc, char *argv[])
         return -1;
     }
 
+    // Get command line arguments
     int num_of_prod = atoi(argv[1]);
     int num_of_cons = atoi(argv[2]);
     num_of_elements = atoi(argv[3]);
 
-    //printf("Creating %d producers and %d consumers. There are %d elements in the array.\n", num_of_prod, num_of_cons,num_of_elements);
+    // Set up shared memory for the following...
+    mutex = mmap(NULL, sizeof(struct cs1550_sem), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0); // mutex semaphore
+    full = mmap(NULL, sizeof(struct cs1550_sem), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);  // full semaphore
+    empty = mmap(NULL, sizeof(struct cs1550_sem), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0); // empty semaphore
+    buffer = mmap(NULL, num_of_elements, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0); // shared array to be subject to race condition
 
+    empty->value = num_of_elements; // Start with N number of empty cells in the array
+    mutex->value = 1; // Set the mutex at one in order to establish atomicity of instructions
+    full->value = 0; // Start with 0 full cells in the array
 
-    mutex = mmap(NULL, sizeof(struct cs1550_sem), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
-    full = mmap(NULL, sizeof(struct cs1550_sem), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
-    empty = mmap(NULL, sizeof(struct cs1550_sem), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
-    buffer = mmap(NULL, num_of_elements, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, 0, 0);
-
-    empty->value = num_of_elements;
-    mutex->value = 1;
-    full->value = 0;
-
-
-    int tid = 0;
+    int i = 0;
     int f = 0;
 
-    for(tid = 0; tid < (num_of_prod + num_of_cons); tid++)
+    // Loop around N number of times, where N is the number of producers you want to create plus the number of consumers.
+    for(i = 0; i < (num_of_prod + num_of_cons); i++)
     {
         f = fork();
         if(f == -1)
@@ -64,13 +61,13 @@ int main (int argc, char *argv[])
             printf("Something went very, very, very badly.\n");
             return -1;
         }
-        else if (f == 0) continue;
-        else break;
+        else if (f == 0) continue; // if we're the parent, continue creating more producers and consumers.
+        else break; // otherwise, we're a child so let's become a producer or consumer...
     }
 
-    if((tid < num_of_prod) && (f != 0)) produce();
-    else consume();
-
+    if(f == 0) return 0; // stop execution of the parent because it has already created all producers and consumers.
+    if(i < num_of_prod) produce(); // create producers
+    else consume(); // create consumers
     return 0;
 }
 
@@ -82,16 +79,17 @@ int produce()
 
     while (1)
     {
-        down(empty);
-        down(mutex);
+        down(empty); // check if there are empty cells in our array to put things in
+        down(mutex); // enter critical region
 
         buffer[in] = pitem;
 
-        in = (in + 1) % num_of_elements;
+        in = (in + 1) % num_of_elements; // circular increment
 
         printf("PRODUCING %d at position %d\n", pitem, in);
-        up(mutex);
-        up(full);
+
+        up(mutex); // leave critical region
+        up(full);  // we've not got one more full cell in our array now that we've produced one.
     }
     return 0;
 }
@@ -104,20 +102,22 @@ int consume()
 
     while (1)
     {
-        down(full);
-
-        down(mutex);
+        down(full); // check if there are full cells in the array to take things from
+        down(mutex); // enter critical region
 
         citem = buffer[out];
-        out = (out+1) % num_of_elements;
+
+        out = (out+1) % num_of_elements; // circular increment
 
         printf("CONSUMING %d at position %d\n", citem, out);
-        up(mutex);
-        up(empty);
+
+        up(mutex); // leave critical region
+        up(empty); // we've got one more empty cell in our array now that we've consumer one.
     }
     return 0;
 }
 
+// helper functions to make syscalls
 void down(struct cs1550_sem *sem)
 {
     syscall(326, sem);
