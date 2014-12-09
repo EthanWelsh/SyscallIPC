59f359b413705514a4b0080aec64ffaa3227e88e
EthanWelsh <welshej@gmail.com>
2014-12-09 15:21:47 -0500

    remove unecessary files

--- thot.bash
+++
@@ -1,2 +0,0 @@
-scp /Users/welshej/github/SyscallIPC/ProducerConsumer/ProducerConsumer/QEMU_Stuff/sys.c ejw45@thot.cs.pitt.edu:/u/OSLab/ejw45/other/linux-2.6.23.1/kernel/
-scp /Users/welshej/github/SyscallIPC/ProducerConsumer/ProducerConsumer/prodcons.c ejw45@thot.cs.pitt.edu:/u/OSLab/ejw45/other/
